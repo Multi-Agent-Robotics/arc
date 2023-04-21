a9d8ca30746ad47a6c712de0a95e3a59195492a4
Jens <jens.jens@live.dk>
2023-04-21 20:44:38 +0200

    feat(control): controller refactoring with pid struct and more params

--- arc_base/src/low_level_motor_controller.cpp
+++ arc_base/src/low_level_motor_controller.cpp
@@ -28,6 +28,12 @@ template <typename... Args> String format_topic_path(Args... args) {
 
 namespace arc {
 
+// pid control struct
+struct PID {
+    double p;
+    double i;
+    double d;
+};
 
 using namespace ros2 = rclcpp;
 
@@ -43,16 +49,22 @@ using MotorControllerStatusMsg = arc_msgs::msg::MotorControllerStatus;
 
 class MotorController : public ros2::Node {
   private:
-    double kp_;
-    double ki_;
-    double kd_;
+    // double kp_;
+    // double ki_;
+    // double kd_;
+    PID pid_;
+    PID control_pid_;
+    double max_output_;
     ros2::Time time_prev_;
+    double target_;
+    double actual_;
+    double error_;
     double control_ki_prev_;
-    double error_eRPM_prev_;
+    double error_prev_;
     VescStateStampedMsg vesc_state_;
     String output_mode_;
 
-    Float64Msg msg_speed_;
+    Float64Msg msg_control_;
     MotorControllerStatusMsg msg_status_;
 
     std::function<double(VescStateStampedMsg::SharedPtr)> get_sensor_value_;
@@ -60,7 +72,7 @@ class MotorController : public ros2::Node {
 
     // ros
     ros2::TimerBase::SharedPtr timer_status_;
-    ros2::TimerBase::SharedPtr timer_speed_;
+    ros2::TimerBase::SharedPtr timer_control_;
     Subscriber<VescStateStampedMsg>::SharedPtr vesc_state_sub_;
     Subscriber<Float64Msg>::SharedPtr diff_drive_sub_;
     Publisher<Float64Msg>::SharedPtr motor_pub_;
@@ -69,7 +81,7 @@ class MotorController : public ros2::Node {
   public:
     MotorController()
         : Node("motor_controller"), control_ki_prev_{0.0},
-          error_eRPM_prev_{0.0}, msg_speed_{}, msg_status_{} {
+          error_prev_{0.0}, msg_control_{}, msg_status_{} {
 
         time_prev_ = this->get_clock()->now();
 
@@ -87,14 +99,6 @@ class MotorController : public ros2::Node {
 
         this->declare_parameter("output_mode");
         output_mode_ = this->get_parameter("output_mode").as_string();
-
-        // pid values from param
-        this->declare_parameter("kp");
-        kp_ = this->get_parameter("kp").as_double();
-        this->declare_parameter("ki");
-        ki_ = this->get_parameter("ki").as_double();
-        this->declare_parameter("kd");
-        kd_ = this->get_parameter("kd").as_double();
         // ------------------
 
         if (output_mode_ == "speed") {
@@ -102,26 +106,53 @@ class MotorController : public ros2::Node {
                 return msg->state.speed;
             };
             get_target_value_ = [](Float64Msg::SharedPtr msg) {
-                double value = msg->data;
+                double target_velocity = msg->data;
                 // do conversion from m/s to eRPM
                 double wheel_circumference = 0.15 * 2 * M_PI;
                 double gear_ratio = 1.0 / 4.0;
                 double motor_poles = 14.0;
-                double erpm_per_sec = value / (wheel_circumference *
+                double erpm_per_sec = target_velocity / (wheel_circumference *
                                                gear_ratio * motor_poles / 2.0);
 
                 return value;
             };
+
+            // pid values from param
+            this->declare_parameter("kp_speed");
+            pid_.p = this->get_parameter("kp_speed").as_double();
+            this->declare_parameter("ki_speed");
+            pid_.i = this->get_parameter("ki_speed").as_double();
+            this->declare_parameter("kd_speed");
+            pid_.d = this->get_parameter("kd_speed").as_double();
+
+            // max output from param
+            this->declare_parameter("speed_max");
+            max_output_ = this->get_parameter("speed_max").as_double();
+
         } else if (output_mode_ == "current") {
             get_sensor_value_ = [](VescStateStampedMsg::SharedPtr msg) {
                 return msg->state.current_motor;
             };
             get_target_value_ = [](Float64Msg::SharedPtr msg) {
-                double value = msg->data;
+                double target_velocity = msg->data;
                 // do conversion from m/s to current (amps)
+                double target_current = target_velocity / 10.0; // placeholder conversion factor
 
                 return value;
             };
+
+            // pid values from param
+            this->declare_parameter("kp_current");
+            pid_.p = this->get_parameter("kp_current").as_double();
+            this->declare_parameter("ki_current");
+            pid_.i = this->get_parameter("ki_current").as_double();
+            this->declare_parameter("kd_current");
+            pid_.d = this->get_parameter("kd_current").as_double();
+
+            // max output from param
+            this->declare_parameter("current_max");
+            max_output_ = this->get_parameter("current_max").as_double();
+
         } else {
             RCLCPP_ERROR(
                 this->get_logger(),
@@ -132,11 +163,23 @@ class MotorController : public ros2::Node {
 
         const String motor_prefix = "motor_" + motor_id;
 
+        // subscribers
         const String diff_drive_topic =
             format_topic_path(motor_prefix, "target", "motor", output_mode_);
         diff_drive_sub_ = this->create_subscription<Float64Msg>(
-            diff_drive_topic, 10, std::bind(&MotorController::step, this, _1));
+            diff_drive_topic, 10,
+            [this](Float64Msg::SharedPtr msg) {
+                target_ = get_target_value_(msg);
+                actual_ = get_sensor_value_(&vesc_state_);
+             });
 
+        const String state_topic =
+            format_topic_path(motor_prefix, "sensors", "core");
+        vesc_state_sub_ = this->create_subscription<VescStateStampedMsg>(
+            state_topic, 10,
+            [this](VescStateStampedMsg::SharedPtr msg) { vesc_state_ = *msg; });
+
+        // publishers
         const String motor_topic =
             format_topic_path(motor_prefix, "commands", "motor", output_mode_);
         motor_pub_ = this->create_publisher<Float64Msg>(
@@ -147,49 +190,49 @@ class MotorController : public ros2::Node {
         status_pub_ = this->create_publisher<MotorControllerStatusMsg>(
             status_topic, ros2::QoS(10).reliable());
 
-        const String state_topic =
-            format_topic_path(motor_prefix, "sensors", "core");
-        vesc_state_sub_ = this->create_subscription<VescStateStampedMsg>(
-            state_topic, 10,
-            [this](VescStateStampedMsg::SharedPtr msg) { vesc_state_ = *msg; });
-
+        // controller rate timer
         const auto cooldown = std::chrono::milliseconds(1000 / controller_rate);
-        timer_speed_ = this->create_wall_timer(
-            cooldown, [this]() { motor_pub_->publish(msg_speed_); });
+        // perform a control step at the controller rate
+        timer_controller_ = this->create_wall_timer(
+            cooldown, [this]() { step(); });
+
+        // publish control and status at the controller rate
+        timer_control_ = this->create_wall_timer(
+            cooldown, [this]() {
+                // update message
+                msg_control_.data = control_pid_.p + control_pid_.i + control_pid_.d;
+
+                motor_pub_->publish(msg_control_);
+            });
 
         timer_status_ = this->create_wall_timer(
-            cooldown, [this]() { status_pub_->publish(msg_status_); });
+            cooldown, [this]() {
+                // update message
+                msg_status_.control.target = target_;
+                msg_status_.control.actual = actual_;
+                msg_status_.control.error = error;
+                msg_status_.pid.p = control_pid_.p;
+                msg_status_.pid.i = control_pid_.i;
+                msg_status_.pid.d = control_pid_.d;
+
+                status_pub_->publish(msg_status_);
+            });
     }
 
-    void step(const Float64Msg::SharedPtr msg) {
-        double target_eRPM = msg->data;
-        double actual_eRPM = vesc_state_;
-        // std::printf("target_eRPM: %f, actual_eRPM: %f\n", target_eRPM,
-        // actual_eRPM);
-
+    void step() {
         auto time_now = this->get_clock()->now();
         auto delta_time = (time_now - time_prev_).nanoseconds() * 1e-9;
-        auto error_eRPM = (target_eRPM - actual_eRPM) * delta_time;
-        // std::printf("error_eRPM: %f\n", error_eRPM);
+        error_ = target_ - actual_ * delta_time; // error over the time step
 
-        double control_kp = kp_ * error_eRPM;
-        double control_ki = control_ki_prev_ + ki_ * error_eRPM;
-        double control_kd = (error_eRPM_prev_ - error_eRPM) * kd_;
-        // std::printf("control_kp: %f, control_ki: %f, control_kd: %f\n",
-        // control_kp, control_ki, control_kd);
+        // controller pid output terms
+        control_pid_.p = kp_ * error_;
+        control_pid_.i = control_ki_prev_ + ki_ * error_;
+        control_pid_.d = (error_prev_ - error) * kd_;
 
-        control_ki_prev_ = control_ki;
-        error_eRPM_prev_ = error_eRPM;
+        // update previous values
+        control_ki_prev_ = control_pid_.i;
+        error_prev_ = error_;
         time_prev_ = time_now;
-
-        msg_speed_.data = control_kp + control_ki + control_kd;
-
-        msg_status_.control.target = target_eRPM;
-        msg_status_.control.actual = actual_eRPM;
-        msg_status_.control.error = error_eRPM;
-        msg_status_.pid.p = control_kp;
-        msg_status_.pid.i = control_ki;
-        msg_status_.pid.d = control_kd;
     }
 };
 } // namespace arc
