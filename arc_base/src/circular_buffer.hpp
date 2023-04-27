86f258b44d3611193f9ae3489f470880b2603391
Kristoffer Plagborg Bak Sørensen <kristoffer.pbs@tuta.io>
2023-04-27 20:50:12 +0200

    perf: use a running sum for the moving average to achieve O(1) time complexity

--- arc_base/src/circular_buffer.hpp
+++ arc_base/src/circular_buffer.hpp
@@ -8,63 +8,55 @@
 #include <vector>
 
 template <typename T, std::size_t N> class CircularBuffer {
-public:
-	// Constructor: initializes the buffer with the specified size
-	explicit CircularBuffer() : head(0), tail(0), count(0) {
-	}
+  public:
+    explicit CircularBuffer() : head(0), tail(0), count(0), sum(0.0) {}
 
-	// push_back: adds an element to the buffer, overwriting the oldest element if
-	// the buffer is full
-	auto push_back(const T &value) -> void {
-		buffer[tail] = value;
-		tail = (tail + 1) % N;
+    auto push_back(const T &value) -> void {
+        if (count < N) {
+            sum += value;
+        } else {
+            sum -= buffer[head];
+            sum += value;
+            head = (head + 1) % N;
+        }
 
-		// If the buffer isn't full, increment the count
-		// Otherwise, increment head to maintain the correct start position
-		if (count < N) {
-			++count;
-		} else {
-			head = (head + 1) % N;
-		}
-	}
+        buffer[tail] = value;
+        tail = (tail + 1) % N;
 
-	// operator[]: provides access to elements in the buffer using an index
-	auto operator[](std::size_t index) -> T & {
-		if (index >= count) {
-			throw std::out_of_range("Index out of range.");
-		}
-		return buffer[(head + index) % N];
-	}
+        if (count < N) {
+            ++count;
+        }
+    }
 
-	// const operator[]: provides const access to elements in the buffer using an
-	// index
-	auto operator[](std::size_t index) const -> const T & {
-		if (index >= count) {
-			throw std::out_of_range("Index out of range.");
-		}
-		return buffer[(head + index) % N];
-	}
+    auto operator[](std::size_t index) -> T & {
+        if (index >= count) {
+            throw std::out_of_range("Index out of range.");
+        }
+        return buffer[(head + index) % N];
+    }
 
-	// size: returns the current number of elements in the buffer
-	auto size() const -> std::size_t {
-		return count;
-	}
+    auto operator[](std::size_t index) const -> const T & {
+        if (index >= count) {
+            throw std::out_of_range("Index out of range.");
+        }
+        return buffer[(head + index) % N];
+    }
 
-	auto get_underlying_array() -> const std::array<T, N> & {
-		return buffer;
-	}
+    auto size() const -> std::size_t { return count; }
 
-	auto mean() -> double {
-		if (count == 0) {
-			return 0.0;
-		}
-		auto sum = std::accumulate(buffer.begin(), buffer.end(), 0.0);
-		return sum / static_cast<decltype(*buffer.begin() + 0.0)>(count);
-	}
+    auto get_underlying_array() -> const std::array<T, N> & { return buffer; }
 
-private:
-	std::array<T, N> buffer;
-	std::size_t head;
-	std::size_t tail;
-	std::size_t count;
+    auto mean() -> double {
+        if (count == 0) {
+            return 0.0;
+        }
+        return sum / static_cast<decltype(*buffer.begin() + 0.0)>(count);
+    }
+
+  private:
+    std::array<T, N> buffer;
+    std::size_t head;
+    std::size_t tail;
+    std::size_t count;
+    double sum;
 };
